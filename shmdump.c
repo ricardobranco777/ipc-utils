78b5cebf2faaa2634b571a4c39959a9712fe4d87
Ricardo Branco <ricardo.branco@smartmatic.com>
2016-10-10 21:05:49 -0500

    shmdump: Support IPC namespaces

--- shmdump.c
+++ shmdump.c
@@ -2,16 +2,38 @@
  * (c) 2016 by Ricardo Branco
  * MIT License
  *
- * v1.0
+ * v1.1
  *
  * This programs dumps the memory specified by the SysV IPC Shared Memory ID to the specified file.
+ *
+ * Note:
+ * It supports IPC namespaces. (Linux only). The argument to the -i option is a PID.
+ * See lsns(1) from latest util-linux to list namespaces.
  */
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <libgen.h>
 #include <sys/types.h>
 #include <sys/shm.h>
+#ifdef __linux__
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <sched.h>
+#endif
+
+unsigned long xstrtoul(const char *str, int base);
+
+static char *progname;
+
+static void exit_usage(int status)
+{
+	printf("Usage: %s [-i PID] SHMID DUMPFILE\n", progname);
+	exit(status);
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,18 +41,55 @@ int main(int argc, char *argv[])
 	void *addr;
 	char *file;
 	FILE *out;
+	int opt;
 	int id;
+#ifdef CLONE_NEWIPC
+	char path[PATH_MAX];
+	pid_t fd = -1;
+#endif
 
-	if (argc != 3) {
-		printf("Usage: %s SHMID DUMPFILE\n", argv[0]);
-		exit(1);
+	while ((opt = getopt(argc, argv, ":hi:")) != -1) {
+		switch (opt) {
+		case 'h':
+			exit_usage(0);
+#ifdef CLONE_NEWIPC
+		case 'i':
+		       if (fd != -1)
+			       exit_usage(1);
+		       {
+			       pid_t pid = (pid_t) xstrtoul(optarg, 10);
+			       snprintf(path, PATH_MAX, "/proc/%ld/ns/ipc", (long) pid);
+		       }
+		       fd = open(path, O_RDONLY);
+		       if (fd < 0) {
+			       perror(path);
+			       exit(1);
+		       }
+		       break;
+#endif
+		default:
+			exit_usage(1);
+		}
 	}
 
-	id = atoi(argv[1]);
-	file = argv[2];
+	argc -= optind;
+	argv += optind;
+
+	if (argc < 2)
+		exit_usage(1);
+
+	id = atoi(argv[0]);
+	file = argv[1];
+
+#ifdef CLONE_NEWIPC
+	if (fd != -1 && setns(fd, CLONE_NEWIPC) < 0) {
+		perror("setns()");
+		exit(1);
+	}
+#endif
 
 	if (shmctl(id, IPC_STAT, &info) < 0) {
-		perror(argv[1]);
+		perror(argv[0]);
 		exit(1);
 	}
 
@@ -46,9 +105,9 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	fwrite(addr, info.shm_segsz, 1, out);
+	(void) fwrite(addr, info.shm_segsz, 1, out);
 
-	shmdt(addr);
+	(void) shmdt(addr);
 
 	exit(0);
 }
